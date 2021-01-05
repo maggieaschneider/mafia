cb1142fa321e76975fd7428cdc866734392575f9
Maggie <margaret.anne.schneider@gmail.com>
2021-01-05 13:09:58 -0500

    hey\

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.on('sendMessage', function (data) {
-  socket.broadcast.emit('message', data);
-  socket.emit('message', { text: data.text });   
-  });   
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
+
 
 http.listen(2000, () => {
   console.log('listening on *:2000');
