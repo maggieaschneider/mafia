cab91fcc2ac57ec2c944ac2f493fd03c001b37de
Maggie <margaret.anne.schneider@gmail.com>
2021-01-05 17:02:42 -0500

    yuh

--- index.js
+++ index.js
@@ -21,26 +21,3 @@ http.listen(2000, () => {
 });
  
 
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
- 
-app.get('/', (req, res) => {
- res.sendFile(__dirname + '/index.html');
-});
- 
-io.on('connection', (socket) => {
- console.log('a user connected');
- socket.on('disconnect', () => {
-   console.log('user disconnected');
- });
- socket.on('chat message', (msg) => {
-   io.emit('chat message', msg);
- });
-});
- 
-http.listen(2000, () => {
- console.log('listening on *:2000');
-});
- 
-
