a609295624435c23816a1edb863f5e28367d500e
Maggie <margaret.anne.schneider@gmail.com>
2021-01-06 13:26:03 -0500

    whatsup

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ io.on('connection', (socket) => {
  });
 });
  
-http.listen(3000, () => {
- console.log('listening on *:3000');
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
  
 
