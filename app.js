27f15babebb0beeb826cc59a6f65f65250658183
Maggie <margaret.anne.schneider@gmail.com>
2021-01-06 14:12:26 -0500

    yuh

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-=======
 const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
+    res.sendFile(__dirname + '/index.html');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>rhianna sucks</h1>');
@@ -12,4 +11,3 @@ const server = http.createServer((req, res) => {
 server.listen(port,() => {
   console.log(`Server running at port `+port);
 }); 
->>>>>>> 2d896c4654b5cd3abf6b1c8223e2c681a2719658
