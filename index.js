2a195bfa67469cb853f5c8ca289d5de7c51f6ee3
Maggie <margaret.anne.schneider@gmail.com>
2021-01-06 13:09:15 -0500

    script

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ io.on('connection', (socket) => {
  });
 });
  
-http.listen(2000, () => {
- console.log('listening on *:2000');
+http.listen(3000, () => {
+ console.log('listening on *:3000');
 });
  
 
