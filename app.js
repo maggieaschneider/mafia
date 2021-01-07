9b8e65b6aca31dccc3bebb47727fc3b4815ff0e4
Maggie <margaret.anne.schneider@gmail.com>
2021-01-07 16:26:45 -0500

    ok

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000
-var io = require('socket.io')(http);
 
 const server = http.createServer((req, res) => {
     
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>rhianna sucks</h1>');
+  res.setText('Content-Type', 'text/html');
+  res.end('<h1>yes</h1>');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
- }); 
-
-
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+}); 
