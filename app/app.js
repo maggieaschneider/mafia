e6139a17fac6d2a50fe7a7b963ab339caed6371d
Maggie <margaret.anne.schneider@gmail.com>
2021-01-08 13:34:00 -0500

    app/app

---
+++ app/app.js
@@ -0,0 +1,29 @@
+const port = process.env.PORT || 3000
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+// const server = http.createServer((req, res) => {
+    
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<h1>we love you rhianna</h1>');
+//   res.end('hello');
+  
+// });
+ 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+ }); 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+ });
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+}); 
