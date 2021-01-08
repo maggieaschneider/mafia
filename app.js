b6242e834003f7d050d34213ef6eb28ac670b75e
Maggie <margaret.anne.schneider@gmail.com>
2021-01-08 13:10:26 -0500

    ok

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const server = http.createServer((req, res) => {
     
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+  res.write('hello');
   res.end('<h1>we love you rhianna</h1>');
+  
 });
 
 server.listen(port,() => {
