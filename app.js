e51462773d033b46a1eace13f4912675731e27d3
Maggie <margaret.anne.schneider@gmail.com>
2021-01-08 13:21:27 -0500

    update app

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const http = require('http');
 const port = process.env.PORT || 3000
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
     
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('hello');
-  res.end('<h1>we love you rhianna</h1>');
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<h1>we love you rhianna</h1>');
+//   res.end('hello');
   
-});
-
-server.listen(port,() => {
+// });
+ 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+ }); 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+ });
+http.listen(port,() => {
   console.log(`Server running at port `+port);
 }); 
