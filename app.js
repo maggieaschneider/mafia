112167db03d4069881dda02143335bb3d89db6f7
Maggie <margaret.anne.schneider@gmail.com>
2021-01-11 13:07:59 -0500

    hello

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ const server = http.createServer((req, res) => {
   res.end('hello');
   
 });
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    // socket.on('chat message', (msg) => {
-    //     io.emit('chat message', msg);
-    //   });
-});
+
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
