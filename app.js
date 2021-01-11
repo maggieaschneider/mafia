668dff9a7b8bd3a6ffa745ad32b6d470c9afd779
Maggie <margaret.anne.schneider@gmail.com>
2021-01-11 13:14:11 -0500

    i give up

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
+const http = require('http');
 const port = process.env.PORT || 3000
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(http);
+// var io = require('socket.io')(http);
 
-// const server = http.createServer((req, res) => {
+
+const server = http.createServer((req, res) => {
     
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.write('<h1>we love you rhianna</h1>');
-//   res.end('hello');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>we love you rhianna</h1>');
+  res.end('hello');
   
-// });
- 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
- }); 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
- });
+});
+
+
 server.listen(port,() => {
   console.log(`Server running at port `+port);
 }); 
---
+++ newapp.js
@@ -0,0 +1,29 @@
+const port = process.env.PORT || 3000
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// const server = http.createServer((req, res) => {
+    
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<h1>we love you rhianna</h1>');
+//   res.end('hello');
+  
+// });
+ 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+ }); 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+ });
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+}); 
