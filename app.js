365916acc106330ab2c2115f686793c19829454b
Maggie <margaret.anne.schneider@gmail.com>
2021-01-11 13:06:50 -0500

    delete var

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000
-var io = require('socket.io')(http);
+// var io = require('socket.io')(http);
 
 
 const server = http.createServer((req, res) => {
